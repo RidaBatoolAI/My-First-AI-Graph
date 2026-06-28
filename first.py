cbb36ef97f4cf63c42385b489253651b4a5ec80a
RidaBatoolAI <ridabatol512@gmail.com>
2026-06-28 15:14:25 +0500

    Refactor histogram and title display in first.py

--- first.py
+++ first.py
@@ -6,10 +6,10 @@ marks = np.random.randint(0, 100, 1000)
 print("Average Mark hai:", marks.mean()) 
 print("Sab se bara Mark hai:", marks.max()) 
 
-# YE LINE CHANGE KAR DE BAS 👇
-plt.hist(marks, bins=20, color='hotpink', edgecolor='black') # Pink kar diya + black border
 
-plt.title("Rida ki CSC Class - Marks ka Graph") # Title daal diya
+plt.hist(marks, bins=20, color='hotpink', edgecolor='black') 
+
+plt.title("Rida ki CSC Class - Marks ka Graph") 
 plt.xlabel("Marks") 
 plt.ylabel("Kitne Students") 
 plt.show() 
